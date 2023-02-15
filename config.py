6360d5cda1ade9f76be460364948c8049e95ef0b
winndy8 <125177960+winndy8@users.noreply.github.com>
2023-02-15 21:25:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ class Config:
     DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://clone:clone@cluster0.go11yez.mongodb.net/?retryWrites=true&w=majority")
     DATABASE_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
     COLLECTION_NAME = os.environ.get('COLLECTION_NAME', 'Forward_Files')
-    SESSION = os.environ.get("SESSION", "")
+    SESSION = os.environ.get("SESSION", "BQGDMIkAAGV9w3L6fKhpL9D0uOBVE_SyHJwBvn1mLpBxTAzVxptEz4Ah-H8VIextUZZL0tpnZ8BFdBLYK5UPK-mOKAREBvkob-QyAaL4PR9AxhtzOjJpzK5_HoejbSFDHYNNxVB2slcAvvZUK2hw58ekIXOyoivSFcJZdoPRWhh10f5rKT59tWi2etv_OZ9XuhPIoVzs5fkZhwzzm_ILOlYWzF-iIR4tdponznGjgdUGegnVO8ZOMFqXRd4l609346aGReU1OmjO0cQl4fBmLxbhrLwiDbwZcFLBxUh2HQUleX5Nxo9ltTWb1V-1z0L3WL6540Sc0JUoQYmGJS5pVfzNzlYwAAAAFUPt0PAA")
     TO_CHANNEL = int(os.environ.get("TO_CHANNEL", "-1001676349938"))
     BOT_USERNAME= os.environ.get("BOT_USERNAME", "wadefrtbot")
 
